42b7744723275bad4aef08034ca6a32bd21f2568
Brent Guistwite <brent.guistwite@flosports.tv>
2024-12-02 14:02:11 -0500

    remove fakeData

--- days/2/part1.js
+++ days/2/part1.js
@@ -13,22 +13,14 @@ import inputs from './inputs.js';
 
 function isAllDecreasing(report) {
     let isAllDecreasing = true;
-    let badLevelCount = 0;
 
     for (let i = 0; i < report.length - 1; i++) {
         const first = report[i];
         const second = report[i + 1];
 
         if (first === second || first < second || Math.abs(first - second) > 3) {
-            badLevelCount++;
-
-            console.log('(isAllDecreasing)badLevelCount', badLevelCount);
-            console.log('(isAllDecreasing)report', report);
-
-            if (badLevelCount > 1) {
-                isAllDecreasing = false;
-                break;
-            }
+            isAllDecreasing = false;
+            break;
         }
     }
     return isAllDecreasing;
@@ -36,22 +28,14 @@ function isAllDecreasing(report) {
 
 function isAllIncreasing(report) {
     let isAllIncreasing = true;
-    let badLevelCount = 0;
 
     for (let i = 0; i < report.length - 1; i++) {
         const first = report[i];
         const second = report[i + 1];
 
         if (first === second || first > second || Math.abs(second - first) > 3) {
-            badLevelCount++;
-
-            console.log('(isAllIncreasing)badLevelCount', badLevelCount);
-            console.log('(isAllIncreasing)report', report);
-
-            if (badLevelCount > 1) {
-                isAllIncreasing = false;
-                break;
-            }
+            isAllIncreasing = false;
+            break;
         }
     }
     return isAllIncreasing;
@@ -66,13 +50,12 @@ function isSafe(data) {
         if (isAllDecreasing(report) || isAllIncreasing(report)) {
             console.log('report is safe', report)
             safeCount++;
+        } else {
+            console.log('report is not safe', report)
         }
+
     }
     return safeCount;
 }
 
-console.log(isSafe(inputs.sm));
-// console.log(isSafe('1 3 2 4 5'));
-
-// console.log(isAllIncreasing([1, 3, 2, 4, 5]));
-// console.log(isAllDecreasing([1, 3, 2, 4, 5]));
+console.log('answer is should be 510 - ', isSafe(inputs.lg));
